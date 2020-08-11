7cc4b232351c36482c818d40d05d4c71fc6c11fd
powsikan <powsi1997@gmail.com>
2020-08-11 14:14:08 +0530

    swagger configurations

--- src/main/java/com/smartzi/springbootswagger/SpringbootSwaggerApplication.java
+++ src/main/java/com/smartzi/springbootswagger/SpringbootSwaggerApplication.java
@@ -2,7 +2,17 @@ package com.smartzi.springbootswagger;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.Collection;
+import java.util.Collections;
 
 @SpringBootApplication
 @EnableSwagger2
@@ -12,4 +22,25 @@ public class SpringbootSwaggerApplication {
         SpringApplication.run(SpringbootSwaggerApplication.class, args);
     }
 
+    @Bean
+    public Docket swaggerConfiguration(){
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .paths(PathSelectors.ant("/api//*"))
+                .apis(RequestHandlerSelectors.basePackage("com.smartzi"))
+                .build()
+                .apiInfo(apiDetails());
+    }
+
+    private ApiInfo apiDetails() {
+        return new ApiInfo(
+                "Book REST API",
+                "Spring Boot REST API for swagger integration",
+                "1.0",
+                "Free to use",
+                new springfox.documentation.service.Contact("Powsikan", "https://smartzi.com", "powsikan@smartzi.com"),
+                "API License",
+                "https://www.smartzi.com",
+                Collections.emptyList());
+    }
 }
