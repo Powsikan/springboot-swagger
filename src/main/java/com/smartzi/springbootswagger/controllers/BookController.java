cd78f3d95c48f887c080161d77a3c0c3e656799f
powsikan <powsi1997@gmail.com>
2020-08-11 13:01:38 +0530

    end points added

--- src/main/java/com/smartzi/springbootswagger/controllers/BookController.java
+++ src/main/java/com/smartzi/springbootswagger/controllers/BookController.java
@@ -1,4 +1,26 @@
 package com.smartzi.springbootswagger.controllers;
 
+import com.smartzi.springbootswagger.models.Book;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
 public class BookController {
+
+    @GetMapping("/{id}")
+    public Book getBook(@PathVariable int id){
+
+    }
+
+    @GetMapping("/")
+    public List<Book> getAllBooks(){
+
+    }
+
+    @PostMapping("/")
+    public Book addBook(@RequestBody Book book){
+
+    }
 }
